af99477a095d4457aabab13479e65ce3fe2e2b3c
MatheusBrandao525 <mafe123silva@gmail.com>
2023-04-16 10:03:28 -0300

    Planejamento do banco de dados completado.

--- db/database.sql
+++ db/database.sql
@@ -5,7 +5,7 @@ CREATE TABLE tbl_categoria (
   categoria_id INT AUTO_INCREMENT PRIMARY KEY,
   nome VARCHAR(100) NOT NULL,
   descricao TEXT,
-  imagem VARCHAR(255)
+  imagem VARCHAR(110)
 )DEFAULT CHARSET=utf8mb4;
 
 
@@ -13,9 +13,9 @@ CREATE TABLE tbl_produto (
   produto_id INT AUTO_INCREMENT PRIMARY KEY,
   nome VARCHAR(100) NOT NULL,
   descricao TEXT NOT NULL,
-  custo INT NOT NULL,
-  preco FLOAT NOT NULL,
-  preco_promocional FLOAT,
+  custo DECIMAL(10,2) NOT NULL,
+  preco DECIMAL(10,2) NOT NULL,
+  preco_promocional DECIMAL(10,2) NULL,
   quantidade INT NOT NULL,
   imagem VARCHAR(255),
   tem_cores ENUM('Sim', 'Não') NOT NULL,
@@ -30,7 +30,7 @@ CREATE TABLE tbl_produto (
 CREATE TABLE tbl_produto_imagem (
   id INT AUTO_INCREMENT PRIMARY KEY,
   produto_id INT NOT NULL,
-  caminho VARCHAR(255) NOT NULL,
+  imagem_prod VARCHAR(110) NOT NULL,
   FOREIGN KEY (produto_id) REFERENCES tbl_produto(produto_id) ON DELETE CASCADE
 )DEFAULT CHARSET=utf8mb4;
 
@@ -38,6 +38,7 @@ CREATE TABLE tbl_produto_imagem (
 CREATE TABLE tbl_cores (
   cor_id INT AUTO_INCREMENT PRIMARY KEY,
   nome_cor VARCHAR(50) NOT NULL,
+  codigo_barras VARCHAR(20) NOT NULL,
   codigo_hex VARCHAR(7) NOT NULL,
   produto_id INT NOT NULL,
   FOREIGN KEY (produto_id) REFERENCES tbl_produtos (produto_id)
@@ -47,6 +48,7 @@ CREATE TABLE tbl_cores (
 CREATE TABLE tbl_tamanhos (
   tamanho_id INT AUTO_INCREMENT PRIMARY KEY,
   nome_tamanho VARCHAR(24) NOT NULL,
+  codigo_barras VARCHAR(20) NOT NULL,
   produto_id INT NOT NULL,
   FOREIGN KEY (produto_id) REFERENCES tbl_produtos (produto_id)
 )DEFAULT CHARSET=utf8mb4;
@@ -57,10 +59,11 @@ CREATE TABLE tbl_usuario (
   usuario_id INT AUTO_INCREMENT PRIMARY KEY,
   nome VARCHAR(100) NOT NULL,
   sobrenome VARCHAR(100) NOT NULL,
-  num_cpf VARCHAR(11) UNIQUE NOT NULL,
+  tipo_cadastro ENUM ("Pessoa Fisica", "Pessoa Juridica") NOT NULL DEFAULT 'Pessoa Fisica',
+  num_cpf VARCHAR(14) UNIQUE NOT NULL,
   email VARCHAR(100) NOT NULL UNIQUE,
-  genero ENUM ("Homem", "Mulher") NOT NULL,
-  senha VARCHAR(100) NOT NULL,
+  genero ENUM ("Homem", "Mulher", "Outros") NOT NULL,
+  senha VARCHAR(8) NOT NULL,
   tipo ENUM ('ADMIN', 'COMUM') NOT NULL DEFAULT 'COMUM',
   telefone VARCHAR(15) NOT NULL,
   pais VARCHAR(80) NOT NULL,
@@ -98,11 +101,10 @@ CREATE TABLE tbl_lista_desejos (
 
 CREATE TABLE tbl_promocao (
   promocao_id INT AUTO_INCREMENT PRIMARY KEY,
-  banner_url VARCHAR(255) NOT NULL,
+  banner_url VARCHAR(110) NOT NULL,
   data_inicio DATETIME NOT NULL,
   data_fim DATETIME NOT NULL,
   descricao TEXT NOT NULL,
-  desconto INT NOT NULL,
   ativo BOOLEAN NOT NULL DEFAULT true
 )DEFAULT CHARSET=utf8mb4;
 
@@ -110,6 +112,7 @@ CREATE TABLE tbl_promocao (
 CREATE TABLE tbl_promocao_produto (
   promocao_id INT NOT NULL,
   produto_id INT NOT NULL,
+  desconto INT NOT NULL,
   PRIMARY KEY (promocao_id, produto_id),
   FOREIGN KEY (promocao_id) REFERENCES tbl_promocao(promocao_id),
   FOREIGN KEY (produto_id) REFERENCES tbl_produto(produto_id)
@@ -119,13 +122,14 @@ CREATE TABLE tbl_promocao_produto (
 CREATE TABLE tbl_pedido (
   pedido_id INT AUTO_INCREMENT PRIMARY KEY,
   usuario_id INT NOT NULL,
-  endereco_entrega VARCHAR(255) NOT NULL,
+  logradouro VARCHAR(80) NOT NULL,
   num_casa VARCHAR(10) NOT NULL,
   num_cep VARCHAR(8) NOT NULL,
   estado VARCHAR(80) NOT NULL,
   pais VARCHAR(80) NOT NULL,
+  tipo_endereco ENUM ("Residencial", "Comercial") NOT NULL DEFAULT 'Residencial',
   valor_total DECIMAL(10, 2) NOT NULL,
-  forma_pagamento ENUM('CARTAO', 'BOLETO', 'TRANSFERENCIA') NOT NULL,
+  forma_pagamento ENUM('CARTAO', 'BOLETO', 'TRANSFERENCIA', 'NO LOCAL', 'CHEQUE', 'EM ESPECIE') NOT NULL,
   data_pedido DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
   codigo_rastreio VARCHAR(50),
   status ENUM('PENDENTE', 'EM ANDAMENTO', 'FINALIZADO', 'CANCELADO') NOT NULL DEFAULT 'PENDENTE'
@@ -149,7 +153,7 @@ CREATE TABLE tbl_vendedor (
   sobrenome VARCHAR(100) NOT NULL,
   email VARCHAR(100) NOT NULL UNIQUE,
   telefone VARCHAR(15) NOT NULL,
-  cpf VARCHAR(11) NOT NULL UNIQUE,
+  cpf VARCHAR(11) NULL UNIQUE,
   avaliacoes INT NOT NULL DEFAULT 0,
   ativo BOOLEAN NOT NULL DEFAULT true,
   foto_perfil VARCHAR(255) DEFAULT 'default.jpg'
@@ -162,6 +166,7 @@ CREATE TABLE tbl_avaliacao (
   usuario_id INT NOT NULL,
   nota DECIMAL(2,1) NOT NULL,
   comentario TEXT,
+  tem_imagem ENUM ("Sim", "Nao") NOT NULL DEFAULT 'Nao',
   data_avaliacao DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
   FOREIGN KEY (produto_id) REFERENCES tbl_produto(produto_id),
   FOREIGN KEY (usuario_id) REFERENCES tbl_usuario(usuario_id)
@@ -171,7 +176,7 @@ CREATE TABLE tbl_avaliacao (
 CREATE TABLE tbl_imagem_avaliacao (
   imagem_avaliacao_id INT AUTO_INCREMENT PRIMARY KEY,
   avaliacao_id INT NOT NULL,
-  imagem BLOB NOT NULL,
+  imagem VARCHAR(110) NOT NULL,
   FOREIGN KEY (avaliacao_id) REFERENCES tbl_avaliacao(avaliacao_id)
 )DEFAULT CHARSET=utf8mb4;
 
@@ -183,5 +188,5 @@ CREATE TABLE tbl_beneficios (
   data_inicio DATE NOT NULL,
   data_fim DATE NOT NULL,
   descricao TEXT,
-  FOREIGN KEY (usuario_id) REFERENCES tbl_clientes(usuario_id)
+  FOREIGN KEY (usuario_id) REFERENCES tbl_usuario(usuario_id)
 ) DEFAULT CHARSET=utf8mb4;
