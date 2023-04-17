f1efd2416e0759496483c2663fa34d7e6fb8d29a
MatheusBrandao525 <mafe123silva@gmail.com>
2023-04-17 19:49:03 -0300

    feat: Criação de pagina para o usuario solicitar suport dentro da aplicação, front completo.

---
+++ app/pages/perfil/opcoesMenu/suportPerfil.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Atendimento via Chat</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+  <link rel="stylesheet" href="./opcoesMenu/opcoesMenu.css">
+</head>
+<body>
+
+  <div class="container mt-4">
+    <div class="row">
+      <div class="col-sm-4 mb-4 mb4-centro">
+        <div class="card cardSuport">
+            <div style="display: flex; justify-content:center">
+                <img class="card-img-top cardImageSuport rounded-circle" src="https://via.placeholder.com/150" alt="Canal de Suporte 1">
+            </div>
+          <div class="card-body">
+            <h5 class="card-title">Canal de Suporte 1</h5>
+            <p class="card-text text-center">Horário de atendimento: 9h às 18h</p>
+            <div style="display: flex; justify-content:center">
+                <a href="#" class="btn btn-primary">Solicitar Suporte</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-4 mb-4 mb4-centro">
+        <div class="card cardSuport">
+            <div style="display: flex; justify-content:center">
+                <img class="card-img-top cardImageSuport rounded-circle" src="https://via.placeholder.com/150" alt="Canal de Suporte 2">
+            </div>
+          <div class="card-body">
+            <h5 class="card-title">Canal de Suporte 2</h5>
+            <p class="card-text text-center">Horário de atendimento: 24h</p>
+            <div style="display: flex; justify-content:center">
+                <a href="#" class="btn btn-primary">Solicitar Suporte</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-4 mb-4 mb4-centro">
+        <div class="card cardSuport">
+            <div style="display: flex; justify-content:center">
+                <img class="card-img-top cardImageSuport rounded-circle" src="https://via.placeholder.com/150" alt="Canal de Suporte 3">
+            </div>
+          <div class="card-body">
+            <h5 class="card-title">Canal de Suporte 3</h5>
+            <p class="card-text text-center">Horário de atendimento: 8h às 22h</p>
+            <div style="display: flex; justify-content:center">
+                <a href="#" class="btn btn-primary">Solicitar Suporte</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+</body>
+</html>
