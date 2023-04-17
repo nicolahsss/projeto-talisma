850835e210b2afb8a628d5e37884333fa38af7e1
MatheusBrandao525 <mafe123silva@gmail.com>
2023-04-17 19:46:31 -0300

    feat: Criação de pagina para o usuario visualizar seus beneficios. front completo.

---
+++ app/pages/perfil/opcoesMenu/meusBeneficios.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <title>Meus Benefícios</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+  <div class="container">
+    <h1 class="text-center my-5">Seus Benefícios</h1>
+    <div class="row">
+      <div class="col-md-4 mb-4">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Benefício 1</h5>
+            <p class="card-text">Descrição breve do benefício 1.</p>
+            <p class="card-text"><small>Data de expiração: 31/12/2022</small></p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 mb-4">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Benefício 2</h5>
+            <p class="card-text">Descrição breve do benefício 2.</p>
+            <p class="card-text"><small>Data de expiração: 30/06/2023</small></p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 mb-4">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Benefício 3</h5>
+            <p class="card-text">Descrição breve do benefício 3.</p>
+            <p class="card-text"><small>Data de expiração: 15/09/2023</small></p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</body>
+</html>
