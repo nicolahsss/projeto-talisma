67448d648402eb7a10650298ef2023fd040e2674
MatheusBrandao525 <mafe123silva@gmail.com>
2023-04-17 19:43:35 -0300

    feat: Criação de pagina para exibição de produtos que estão na lista de desejos do usuario.

---
+++ app/pages/perfil/opcoesMenu/meusDesejos.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Produtos no Carrinho</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+	<div class="container-fluid">
+		<h2>Sua lista de desejos</h2>
+		<hr>
+		<div class="row">
+			<div class="col-md-3">
+				<div class="card">
+					<img class="card-img-top" src="https://via.placeholder.com/150" alt="Imagem do Produto">
+					<div class="card-body">
+						<h5 class="card-title">Nome do Produto</h5>
+						<p class="card-text">Descrição breve do produto.</p>
+						<p class="card-text">R$ 99,00</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-3">
+				<div class="card">
+					<img class="card-img-top" src="https://via.placeholder.com/150" alt="Imagem do Produto">
+					<div class="card-body">
+						<h5 class="card-title">Nome do Produto</h5>
+						<p class="card-text">Descrição breve do produto.</p>
+						<p class="card-text">R$ 199,00</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-3">
+				<div class="card">
+					<img class="card-img-top" src="https://via.placeholder.com/150" alt="Imagem do Produto">
+					<div class="card-body">
+						<h5 class="card-title">Nome do Produto</h5>
+						<p class="card-text">Descrição breve do produto.</p>
+						<p class="card-text">R$ 299,00</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-3">
+				<div class="card">
+					<img class="card-img-top" src="https://via.placeholder.com/150" alt="Imagem do Produto">
+					<div class="card-body">
+						<h5 class="card-title">Nome do Produto</h5>
+						<p class="card-text">Descrição breve do produto.</p>
+						<p class="card-text">R$ 399,00</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="row mt-4">
+			<div class="col text-center">
+				<a href="carrinho.html" class="btn btn-primary">Ir para o lista de desejos</a>
+			</div>
+		</div>
+	</div>
+
+</body>
+</html>
