a722264f43dff5e0fc8738073b8d7a3230bffe87
MatheusBrandao525 <mafe123silva@gmail.com>
2023-04-17 19:47:24 -0300

    feat: Criação de pagina para o usuario ter acesso a suas atividades dentro do site, front completo.

---
+++ app/pages/perfil/opcoesMenu/minhasAtividades.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Minhas Avaliações e Comentários</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+	<div class="container-fluid">
+		<h2>Sua Atividade</h2>
+		<hr>
+		<div class="row">
+			<div class="col-md-6">
+				<h3>Avaliações</h3>
+				<div class="card">
+					<div class="card-body">
+						<h5 class="card-title">Avaliação de Produto</h5>
+						<p class="card-text">Descrição breve da avaliação.</p>
+						<p class="card-text"><small class="text-muted">22 de março de 2023</small></p>
+					</div>
+				</div>
+				<div class="card">
+					<div class="card-body">
+						<h5 class="card-title">Avaliação de Produto</h5>
+						<p class="card-text">Descrição breve da avaliação.</p>
+						<p class="card-text"><small class="text-muted">28 de janeiro de 2023</small></p>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-6">
+				<h3>Comentários</h3>
+				<div class="card">
+					<div class="card-body">
+						<h5 class="card-title">Comentário de Produto</h5>
+						<p class="card-text">Descrição breve do comentário.</p>
+						<p class="card-text"><small class="text-muted">10 de fevereiro de 2023</small></p>
+					</div>
+				</div>
+				<div class="card">
+					<div class="card-body">
+						<h5 class="card-title">Comentário de Produto</h5>
+						<p class="card-text">Descrição breve do comentário.</p>
+						<p class="card-text"><small class="text-muted">15 de dezembro de 2022</small></p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+</body>
+</html>
