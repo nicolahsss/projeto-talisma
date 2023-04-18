e8d9c801d558ad876bed1b97d87c46ad66d7ba8d
MatheusBrandao525 <mafe123silva@gmail.com>
2023-04-18 18:29:57 -0300

    feat: Front-end completo, pagina de listar usuario.

---
+++ app/pages/painelADM/opcoesPainelADM/opcoesGerenciaClientes/listarClientes.php
@@ -0,0 +1,217 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Avaliações de Clientes</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+<body>
+  <div class="container" style="padding: 20px;">
+
+<div class="row" style="overflow: auto; max-height: 620px;">
+  <div class="col-md-12">
+    <div class="card">
+      <div class="card-body">
+        <div class="row">
+          <div class="col-md-1">
+            <img src="../../../public/assets/img/perfil.jpg" class="rounded-circle" alt="Foto de Perfil" style="width:50px; height:50px;" >
+          </div>
+          <div class="col-md-11" style=" display: flex; align-items:center; justify-content:space-around;">
+            <h5 class="card-title" style="margin-bottom: 0 !important;">Nome do Cliente</h5>
+            <h6 class="card-text" style="margin-bottom: 0 !important;">CPF: xxx.xxx.xxx-xx</h6>
+            <h6 class="card-text" style="margin-bottom: 0 !important;">E-mail: emaildocliente@exemplo.com</h6>
+            <h6 class="card-text" style="margin-bottom: 0 !important;">Telefone: (00) 00000-0000</h6>
+            <button class="btn btn-info">Detalhes</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="col-md-12">
+    <div class="card">
+      <div class="card-body">
+        <div class="row">
+          <div class="col-md-1">
+            <img src="../../../public/assets/img/perfil.jpg" class="rounded-circle" alt="Foto de Perfil" style="width:50px; height:50px;" >
+          </div>
+          <div class="col-md-11" style=" display: flex; align-items:center; justify-content:space-around;">
+            <h5 class="card-title" style="margin-bottom: 0 !important;">Nome do Cliente</h5>
+            <h6 class="card-text" style="margin-bottom: 0 !important;">CPF: xxx.xxx.xxx-xx</h6>
+            <h6 class="card-text" style="margin-bottom: 0 !important;">E-mail: emaildocliente@exemplo.com</h6>
+            <h6 class="card-text" style="margin-bottom: 0 !important;">Telefone: (00) 00000-0000</h6>
+            <button class="btn btn-info">Detalhes</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="col-md-12">
+    <div class="card">
+      <div class="card-body">
+        <div class="row">
+          <div class="col-md-1">
+            <img src="../../../public/assets/img/perfil.jpg" class="rounded-circle" alt="Foto de Perfil" style="width:50px; height:50px;" >
+          </div>
+          <div class="col-md-11" style=" display: flex; align-items:center; justify-content:space-around;">
+            <h5 class="card-title" style="margin-bottom: 0 !important;">Nome do Cliente</h5>
+            <h6 class="card-text" style="margin-bottom: 0 !important;">CPF: xxx.xxx.xxx-xx</h6>
+            <h6 class="card-text" style="margin-bottom: 0 !important;">E-mail: emaildocliente@exemplo.com</h6>
+            <h6 class="card-text" style="margin-bottom: 0 !important;">Telefone: (00) 00000-0000</h6>
+            <button class="btn btn-info">Detalhes</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="col-md-12">
+    <div class="card">
+      <div class="card-body">
+        <div class="row">
+          <div class="col-md-1">
+            <img src="../../../public/assets/img/perfil.jpg" class="rounded-circle" alt="Foto de Perfil" style="width:50px; height:50px;" >
+          </div>
+          <div class="col-md-11" style=" display: flex; align-items:center; justify-content:space-around;">
+            <h5 class="card-title" style="margin-bottom: 0 !important;">Nome do Cliente</h5>
+            <h6 class="card-text" style="margin-bottom: 0 !important;">CPF: xxx.xxx.xxx-xx</h6>
+            <h6 class="card-text" style="margin-bottom: 0 !important;">E-mail: emaildocliente@exemplo.com</h6>
+            <h6 class="card-text" style="margin-bottom: 0 !important;">Telefone: (00) 00000-0000</h6>
+            <button class="btn btn-info">Detalhes</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="col-md-12">
+    <div class="card">
+      <div class="card-body">
+        <div class="row">
+          <div class="col-md-1">
+            <img src="../../../public/assets/img/perfil.jpg" class="rounded-circle" alt="Foto de Perfil" style="width:50px; height:50px;" >
+          </div>
+          <div class="col-md-11" style=" display: flex; align-items:center; justify-content:space-around;">
+            <h5 class="card-title" style="margin-bottom: 0 !important;">Nome do Cliente</h5>
+            <h6 class="card-text" style="margin-bottom: 0 !important;">CPF: xxx.xxx.xxx-xx</h6>
+            <h6 class="card-text" style="margin-bottom: 0 !important;">E-mail: emaildocliente@exemplo.com</h6>
+            <h6 class="card-text" style="margin-bottom: 0 !important;">Telefone: (00) 00000-0000</h6>
+            <button class="btn btn-info">Detalhes</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="col-md-12">
+    <div class="card">
+      <div class="card-body">
+        <div class="row">
+          <div class="col-md-1">
+            <img src="../../../public/assets/img/perfil.jpg" class="rounded-circle" alt="Foto de Perfil" style="width:50px; height:50px;" >
+          </div>
+          <div class="col-md-11" style=" display: flex; align-items:center; justify-content:space-around;">
+            <h5 class="card-title" style="margin-bottom: 0 !important;">Nome do Cliente</h5>
+            <h6 class="card-text" style="margin-bottom: 0 !important;">CPF: xxx.xxx.xxx-xx</h6>
+            <h6 class="card-text" style="margin-bottom: 0 !important;">E-mail: emaildocliente@exemplo.com</h6>
+            <h6 class="card-text" style="margin-bottom: 0 !important;">Telefone: (00) 00000-0000</h6>
+            <button class="btn btn-info">Detalhes</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="col-md-12">
+    <div class="card">
+      <div class="card-body">
+        <div class="row">
+          <div class="col-md-1">
+            <img src="../../../public/assets/img/perfil.jpg" class="rounded-circle" alt="Foto de Perfil" style="width:50px; height:50px;" >
+          </div>
+          <div class="col-md-11" style=" display: flex; align-items:center; justify-content:space-around;">
+            <h5 class="card-title" style="margin-bottom: 0 !important;">Nome do Cliente</h5>
+            <h6 class="card-text" style="margin-bottom: 0 !important;">CPF: xxx.xxx.xxx-xx</h6>
+            <h6 class="card-text" style="margin-bottom: 0 !important;">E-mail: emaildocliente@exemplo.com</h6>
+            <h6 class="card-text" style="margin-bottom: 0 !important;">Telefone: (00) 00000-0000</h6>
+            <button class="btn btn-info">Detalhes</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="col-md-12">
+    <div class="card">
+      <div class="card-body">
+        <div class="row">
+          <div class="col-md-1">
+            <img src="../../../public/assets/img/perfil.jpg" class="rounded-circle" alt="Foto de Perfil" style="width:50px; height:50px;" >
+          </div>
+          <div class="col-md-11" style=" display: flex; align-items:center; justify-content:space-around;">
+            <h5 class="card-title" style="margin-bottom: 0 !important;">Nome do Cliente</h5>
+            <h6 class="card-text" style="margin-bottom: 0 !important;">CPF: xxx.xxx.xxx-xx</h6>
+            <h6 class="card-text" style="margin-bottom: 0 !important;">E-mail: emaildocliente@exemplo.com</h6>
+            <h6 class="card-text" style="margin-bottom: 0 !important;">Telefone: (00) 00000-0000</h6>
+            <button class="btn btn-info">Detalhes</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="col-md-12">
+    <div class="card">
+      <div class="card-body">
+        <div class="row">
+          <div class="col-md-1">
+            <img src="../../../public/assets/img/perfil.jpg" class="rounded-circle" alt="Foto de Perfil" style="width:50px; height:50px;" >
+          </div>
+          <div class="col-md-11" style=" display: flex; align-items:center; justify-content:space-around;">
+            <h5 class="card-title" style="margin-bottom: 0 !important;">Nome do Cliente</h5>
+            <h6 class="card-text" style="margin-bottom: 0 !important;">CPF: xxx.xxx.xxx-xx</h6>
+            <h6 class="card-text" style="margin-bottom: 0 !important;">E-mail: emaildocliente@exemplo.com</h6>
+            <h6 class="card-text" style="margin-bottom: 0 !important;">Telefone: (00) 00000-0000</h6>
+            <button class="btn btn-info">Detalhes</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="col-md-12">
+    <div class="card">
+      <div class="card-body">
+        <div class="row">
+          <div class="col-md-1">
+            <img src="../../../public/assets/img/perfil.jpg" class="rounded-circle" alt="Foto de Perfil" style="width:50px; height:50px;" >
+          </div>
+          <div class="col-md-11" style=" display: flex; align-items:center; justify-content:space-around;">
+            <h5 class="card-title" style="margin-bottom: 0 !important;">Nome do Cliente</h5>
+            <h6 class="card-text" style="margin-bottom: 0 !important;">CPF: xxx.xxx.xxx-xx</h6>
+            <h6 class="card-text" style="margin-bottom: 0 !important;">E-mail: emaildocliente@exemplo.com</h6>
+            <h6 class="card-text" style="margin-bottom: 0 !important;">Telefone: (00) 00000-0000</h6>
+            <button class="btn btn-info">Detalhes</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="col-md-12">
+    <div class="card">
+      <div class="card-body">
+        <div class="row">
+          <div class="col-md-1">
+            <img src="../../../public/assets/img/perfil.jpg" class="rounded-circle" alt="Foto de Perfil" style="width:50px; height:50px;" >
+          </div>
+          <div class="col-md-11" style=" display: flex; align-items:center; justify-content:space-around;">
+            <h5 class="card-title" style="margin-bottom: 0 !important;">Nome do Cliente</h5>
+            <h6 class="card-text" style="margin-bottom: 0 !important;">CPF: xxx.xxx.xxx-xx</h6>
+            <h6 class="card-text" style="margin-bottom: 0 !important;">E-mail: emaildocliente@exemplo.com</h6>
+            <h6 class="card-text" style="margin-bottom: 0 !important;">Telefone: (00) 00000-0000</h6>
+            <button class="btn btn-info">Detalhes</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+  </div>
+</body>
+</html>
