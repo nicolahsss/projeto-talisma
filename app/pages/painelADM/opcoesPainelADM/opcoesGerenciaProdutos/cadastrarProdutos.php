cdb272b803c1ca53a76a3be6b11e2e393990e1ea
MatheusBrandao525 <mafe123silva@gmail.com>
2023-04-22 10:46:00 -0400

    Alteração no formulario de cadastro de produtos, adicionando tamanho e cores disponiveis.

--- app/pages/painelADM/opcoesPainelADM/opcoesGerenciaProdutos/cadastrarProdutos.php
+++ app/pages/painelADM/opcoesPainelADM/opcoesGerenciaProdutos/cadastrarProdutos.php
@@ -18,17 +18,17 @@ require '../../../../../adm/consultasSQL/consultaDadosCategorias.php';
     <form class="row" action="../../../adm/validacoes/validarCadastroDeProduto/cadastrarProduto.php" method="post"  enctype="multipart/form-data">
       <div class="form-group col-md-3">
         <label for="nome">Nome do Produto</label>
-        <input type="text" class="form-control" id="nome" name="nomeproduto" placeholder="Nome do produto">
+        <input type="text" class="form-control" id="nome" name="nomeproduto">
       </div>
 
       <div class="form-group col-md-3">
         <label for="preco-promocional">Codigo de barras</label>
-        <input type="number" class="form-control" name="codigodebarras" id="preco-promocional" placeholder="Preço promocional do produto">
+        <input type="number" class="form-control" name="codigodebarras" id="preco-promocional">
       </div>
 
       <div class="form-group col-md-3">
         <label for="quantidade">Quantidade em estoque</label>
-        <input type="number" class="form-control" name="quantidadeemestoque" id="quantidade" placeholder="Quantidade em estoque do produto">
+        <input type="number" class="form-control" name="quantidadeemestoque" id="quantidade">
       </div>
 
       <div class="form-group col-md-3">
@@ -75,8 +75,37 @@ require '../../../../../adm/consultasSQL/consultaDadosCategorias.php';
         </div>
       </div>
 
-      <div class="col-md-12">
-        <label for="cores">Cores Disponíveis:</label><br>
+      <div class="col-md-12" style="margin-top: 1rem;">
+        <label for="cores">Cores Disponíveis</label>
+      </div>
+      <div class="col-md-3">
+        <input type="checkbox" id="cores" name="cores[Vermelho]" value="">Vermelho<br>
+        <input type="checkbox" id="cores" name="cores[Azul]" value="">Azul<br>
+        <input type="checkbox" id="cores" name="cores[Verde]" value="">Verde<br>
+        <input type="checkbox" id="cores" name="cores[Roxo]" value="">Roxo<br>
+        <input type="checkbox" id="cores" name="cores[Amarelo]" value="">Amarelo<br>
+        <input type="checkbox" id="cores" name="cores[Branco]" value="">Branco<br>
+        <input type="checkbox" id="cores" name="cores[Laranja]" value="">Laranja<br>
+      </div>
+      <div class="col-md-3">
+        <input type="checkbox" id="cores" name="cores[Vermelho]" value="">Vermelho<br>
+        <input type="checkbox" id="cores" name="cores[Azul]" value="">Azul<br>
+        <input type="checkbox" id="cores" name="cores[Verde]" value="">Verde<br>
+        <input type="checkbox" id="cores" name="cores[Roxo]" value="">Roxo<br>
+        <input type="checkbox" id="cores" name="cores[Amarelo]" value="">Amarelo<br>
+        <input type="checkbox" id="cores" name="cores[Branco]" value="">Branco<br>
+        <input type="checkbox" id="cores" name="cores[Laranja]" value="">Laranja<br>
+      </div>
+      <div class="col-md-3">
+        <input type="checkbox" id="cores" name="cores[Vermelho]" value="">Vermelho<br>
+        <input type="checkbox" id="cores" name="cores[Azul]" value="">Azul<br>
+        <input type="checkbox" id="cores" name="cores[Verde]" value="">Verde<br>
+        <input type="checkbox" id="cores" name="cores[Roxo]" value="">Roxo<br>
+        <input type="checkbox" id="cores" name="cores[Amarelo]" value="">Amarelo<br>
+        <input type="checkbox" id="cores" name="cores[Branco]" value="">Branco<br>
+        <input type="checkbox" id="cores" name="cores[Laranja]" value="">Laranja<br>
+      </div>
+      <div class="col-md-3">
         <input type="checkbox" id="cores" name="cores[Vermelho]" value="">Vermelho<br>
         <input type="checkbox" id="cores" name="cores[Azul]" value="">Azul<br>
         <input type="checkbox" id="cores" name="cores[Verde]" value="">Verde<br>
@@ -87,17 +116,60 @@ require '../../../../../adm/consultasSQL/consultaDadosCategorias.php';
       </div>
 
 
-      <div class="form-group col-md-4">
-        <label for="custo">Custo</label>
-        <input type="number" name="custoproduto" class="form-control" id="custo" placeholder="Custo do produto">
+      <div class="col-md-12 row" style="margin-top:1rem;">
+        <div class="form-group col-md-4">
+          <label for="custo">Custo</label>
+          <input type="number" name="custoproduto" class="form-control" id="custo">
+        </div>
+        <div class="form-group col-md-4">
+          <label for="preco">Preço</label>
+          <input type="number" name="precoproduto" class="form-control" id="preco">
+        </div>
+        <div class="form-group col-md-4">
+          <label for="preco-promocional">Preço promocional</label>
+          <input type="number" name="precopromocional" class="form-control" id="preco-promocional">
+        </div>
       </div>
-      <div class="form-group col-md-4">
-        <label for="preco">Preço</label>
-        <input type="number" name="precoproduto" class="form-control" id="preco" placeholder="Preço do produto">
+
+
+      <div class="col-md-12" style="margin-top: 1rem;">
+        <label for="cores">Tamanhos Disponíveis</label>
       </div>
-      <div class="form-group col-md-4">
-        <label for="preco-promocional">Preço promocional</label>
-        <input type="number" name="precopromocional" class="form-control" id="preco-promocional" placeholder="Preço promocional do produto">
+      <div class="col-md-3">
+        <input type="checkbox" id="cores" name="cores[Vermelho]" value="">Vermelho<br>
+        <input type="checkbox" id="cores" name="cores[Azul]" value="">Azul<br>
+        <input type="checkbox" id="cores" name="cores[Verde]" value="">Verde<br>
+        <input type="checkbox" id="cores" name="cores[Roxo]" value="">Roxo<br>
+        <input type="checkbox" id="cores" name="cores[Amarelo]" value="">Amarelo<br>
+        <input type="checkbox" id="cores" name="cores[Branco]" value="">Branco<br>
+        <input type="checkbox" id="cores" name="cores[Laranja]" value="">Laranja<br>
+      </div>
+      <div class="col-md-3">
+        <input type="checkbox" id="cores" name="cores[Vermelho]" value="">Vermelho<br>
+        <input type="checkbox" id="cores" name="cores[Azul]" value="">Azul<br>
+        <input type="checkbox" id="cores" name="cores[Verde]" value="">Verde<br>
+        <input type="checkbox" id="cores" name="cores[Roxo]" value="">Roxo<br>
+        <input type="checkbox" id="cores" name="cores[Amarelo]" value="">Amarelo<br>
+        <input type="checkbox" id="cores" name="cores[Branco]" value="">Branco<br>
+        <input type="checkbox" id="cores" name="cores[Laranja]" value="">Laranja<br>
+      </div>
+      <div class="col-md-3">
+        <input type="checkbox" id="cores" name="cores[Vermelho]" value="">Vermelho<br>
+        <input type="checkbox" id="cores" name="cores[Azul]" value="">Azul<br>
+        <input type="checkbox" id="cores" name="cores[Verde]" value="">Verde<br>
+        <input type="checkbox" id="cores" name="cores[Roxo]" value="">Roxo<br>
+        <input type="checkbox" id="cores" name="cores[Amarelo]" value="">Amarelo<br>
+        <input type="checkbox" id="cores" name="cores[Branco]" value="">Branco<br>
+        <input type="checkbox" id="cores" name="cores[Laranja]" value="">Laranja<br>
+      </div>
+      <div class="col-md-3">
+        <input type="checkbox" id="cores" name="cores[Vermelho]" value="">Vermelho<br>
+        <input type="checkbox" id="cores" name="cores[Azul]" value="">Azul<br>
+        <input type="checkbox" id="cores" name="cores[Verde]" value="">Verde<br>
+        <input type="checkbox" id="cores" name="cores[Roxo]" value="">Roxo<br>
+        <input type="checkbox" id="cores" name="cores[Amarelo]" value="">Amarelo<br>
+        <input type="checkbox" id="cores" name="cores[Branco]" value="">Branco<br>
+        <input type="checkbox" id="cores" name="cores[Laranja]" value="">Laranja<br>
       </div>
         <div class="col-md-12" style="display: flex; justify-content:right;">
             <button class="btn btn-info">Cadastrar</button>
