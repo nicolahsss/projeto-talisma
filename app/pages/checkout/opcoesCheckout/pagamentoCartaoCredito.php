032f3c22f7b32743526c2ad2409e51a2488ef185
MatheusBrandao525 <mafe123silva@gmail.com>
2023-05-05 14:41:04 -0400

    Alterando o front-end do checkout. dados do formulario não estão carregando.

--- app/pages/checkout/opcoesCheckout/pagamentoCartaoCredito.php
+++ app/pages/checkout/opcoesCheckout/pagamentoCartaoCredito.php
@@ -9,9 +9,7 @@ require '../../../../adm/consultasSQL/consultaProdutosNoCarrinho.php';
 <div class="bg-light p-30">
 
 <script src="https://sdk.mercadopago.com/js/v2"></script>
-<script>
-      const mp = new MercadoPago("TEST-e95dc122-b8bc-40dd-8419-ae8f8cb39e40");
-</script>
+
 <style>
     #form-checkout {
       display: flex;
@@ -27,36 +25,66 @@ require '../../../../adm/consultasSQL/consultaProdutosNoCarrinho.php';
       padding: 1px 2px;
     }
   </style>
-  <form id="form-checkout">
-    <div id="form-checkout__cardNumber" name="" class="container"></div>
-    <div id="form-checkout__expirationDate" name="" class="container"></div>
-    <div id="form-checkout__securityCode" name="" class="container"></div>
-    <input type="text" name="" id="form-checkout__cardholderName" />
-    <select id="form-checkout__issuer" name="">
-    <option value="mastercard" name="">Mastercard</option>
-    <option value="visa" name="">Visa</option>
-    <option value="americanexpress" name="">American Express</option>
-  </select>
-  <select id="form-checkout__installments" name="">
-    <option value="1">1x sem juros</option>
-    <option value="2">2x sem juros</option>
-    <option value="3">3x sem juros</option>
-    <option value="4">4x sem juros</option>
-    <option value="5">5x sem juros</option>
-    <option value="6">6x sem juros</option>
-  </select>
-  <select id="form-checkout__identificationType" name="">
-    <option value="cpf">CPF</option>
-    <option value="cnpj">CNPJ</option>
-    <option value="rg">RG</option>
-    <option value="passport">Passaporte</option>
-  </select>
-    <input type="text" name="" id="form-checkout__identificationNumber" />
-    <input type="email" name="" id="form-checkout__cardholderEmail" />
+<input type="hidden" id="mercado-pago-public-key" value="TEST-e95dc122-b8bc-40dd-8419-ae8f8cb39e40">
+<form id="form-checkout">
+                        <h3 class="title">Buyer Details</h3>
+                        <div class="row">
+                            <div class="form-group col">
+                                <input id="form-checkout__cardholderEmail" name="cardholderEmail" type="email" class="form-control"/>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="form-group col-sm-5">
+                                <select id="form-checkout__identificationType" name="identificationType" class="form-control"></select>
+                            </div>
+                            <div class="form-group col-sm-7">
+                                <input id="form-checkout__identificationNumber" name="docNumber" type="text" class="form-control"/>
+                            </div>
+                        </div>
+                        <br>
+                        <h3 class="title">Card Details</h3>
+                        <div class="row">
+                            <div class="form-group col-sm-8">
+                                <input id="form-checkout__cardholderName" name="cardholderName" type="text" class="form-control"/>
+                            </div>
+                            <div class="form-group col-sm-4">
+                                <div class="input-group expiration-date">
+                                    <div id="form-checkout__expirationDate" class="form-control h-40"></div>
+                                </div>
+                            </div>
+                            <div class="form-group col-sm-8">
+                                <div id="form-checkout__cardNumber" class="form-control h-40"></div>
+                            </div>
+                            <div class="form-group col-sm-4">
+                                <div id="form-checkout__securityCode" class="form-control h-40"></div>
+                            </div>
+                            <div id="issuerInput" class="form-group col-sm-12 hidden">
+                                <select id="form-checkout__issuer" name="issuer" class="form-control"></select>
+                            </div>
+                            <div class="form-group col-sm-12">
+                                <select id="form-checkout__installments" name="installments" type="text" class="form-control"></select>
+                            </div>
+                            <div class="form-group col-sm-12">
+                                <input type="hidden" id="amount" />
+                                <input type="hidden" id="description" />
+                                <div id="validation-error-messages">
+                                </div>
+                                <br>
+                                <button id="form-checkout__submit" type="submit" class="btn btn-primary btn-block">Pay</button>
+                                <br>
+                                <p id="loading-message">Loading, please wait...</p>
+                                <br>
+                                <a id="go-back">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 10 10" class="chevron-left">
+                                        <path fill="#009EE3" fill-rule="nonzero"id="chevron_left" d="M7.05 1.4L6.2.552 1.756 4.997l4.449 4.448.849-.848-3.6-3.6z"></path>
+                                    </svg>
+                                    Go back to Shopping Cart
+                                </a>
+                            </div>
+                        </div>
+                    </form>
 
-    <button type="submit" id="form-checkout__submit">Pagar</button>
-    <progress value="0" class="progress-bar">Carregando...</progress>
-  </form>
+
 
 
 
@@ -65,123 +93,148 @@ require '../../../../adm/consultasSQL/consultaProdutosNoCarrinho.php';
 
 
 <script>
+const payButton = document.getElementById("form-checkout__submit");
+const publicKey = document.getElementById("mercado-pago-public-key").value;
+const mercadopago = new MercadoPago(publicKey);
 
-    const cardForm = mp.cardForm({
-      amount: "100.5",
-      iframe: true,
-      form: {
+const form = {
         id: "form-checkout",
-        cardNumber: {
-          id: "form-checkout__cardNumber",
-          placeholder: "Número do cartão",
-        },
-        expirationDate: {
-          id: "form-checkout__expirationDate",
-          placeholder: "MM/YY",
-        },
-        securityCode: {
-          id: "form-checkout__securityCode",
-          placeholder: "Código de segurança",
-        },
         cardholderName: {
-          id: "form-checkout__cardholderName",
-          placeholder: "Titular do cartão",
-        },
-        issuer: {
-          id: "form-checkout__issuer",
-          placeholder: "Banco emissor",
-        },
-        installments: {
-          id: "form-checkout__installments",
-          placeholder: "Parcelas",
-        },        
-        identificationType: {
-          id: "form-checkout__identificationType",
-          placeholder: "Tipo de documento",
-        },
-        identificationNumber: {
-          id: "form-checkout__identificationNumber",
-          placeholder: "Número do documento",
+            id: "form-checkout__cardholderName",
+            placeholder: "Holder name",
         },
         cardholderEmail: {
-          id: "form-checkout__cardholderEmail",
-          placeholder: "E-mail",
+            id: "form-checkout__cardholderEmail",
+            placeholder: "E-mail",
         },
-      },
-      callbacks: {
-        onFormMounted: error => {
-          if (error) return console.warn("Form Mounted handling error: ", error);
-          console.log("Form mounted");
-        },
-        onSubmit: event => {
-          event.preventDefault();
-
-          const {
-            paymentMethodId: payment_method_id,
-            issuerId: issuer_id,
-            cardholderEmail: email,
-            amount,
-            token,
-            installments,
-            identificationNumber,
-            identificationType,
-          } = cardForm.getCardFormData();
-                    console.log({
-            payment_method_id,
-            issuer_id,
-            email,
-            amount,
-            token,
-            installments,
-            identificationNumber,
-            identificationType,
-          });
-
-          fetch("../../../adm/validacoes/pagamento/finalizaPagamentoCartaoCredito.php", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
+        cardNumber: {
+            id: "form-checkout__cardNumber",
+            placeholder: "Card number",
+            style: {
+                fontSize: "1rem"
             },
-            body: JSON.stringify({
-              token,
-              issuer_id,
-              payment_method_id,
-              transaction_amount: Number(amount),
-              installments: Number(installments),
-              description: "Descrição do produto",
-              payer: {
-                email,
-                identification: {
-                  type: identificationType,
-                  number: identificationNumber,
-                },
-              },
-            }),
-          })
-            .then(response => {
-            if (!response.ok) {
-            throw new Error('Falha ao processar pagamento');
-            }
-            console.log(token);
-
-        })
-        .catch(error => {
-            console.error(error);
-
-        });
         },
-        onFetching: (resource) => {
-          console.log("Fetching resource: ", resource);
+        expirationDate: {
+            id: "form-checkout__expirationDate",
+            placeholder: "MM/YYYY",
+            style: {
+                fontSize: "1rem"
+            },
+        },
+        securityCode: {
+            id: "form-checkout__securityCode",
+            placeholder: "Security code",
+            style: {
+                fontSize: "1rem"
+            },
+        },
+        installments: {
+            id: "form-checkout__installments",
+            placeholder: "Installments",
+        },
+        identificationType: {
+            id: "form-checkout__identificationType",
+        },
+        identificationNumber: {
+            id: "form-checkout__identificationNumber",
+            placeholder: "Identification number",
+        },
+        issuer: {
+            id: "form-checkout__issuer",
+            placeholder: "Issuer",
+        },
+    };
 
-          // Animate progress bar
-          const progressBar = document.querySelector(".progress-bar");
-          progressBar.removeAttribute("value");
+    const cardForm = mercadopago.cardForm({
+        amount: 110.98,
+        iframe: true,
+        form,
+        callbacks: {
+            onFormMounted: error => {
+                if (error)
+                    return console.warn("Form Mounted handling error: ", error);
+                console.log("Form mounted");
+            },
+            onSubmit: event => {
+                event.preventDefault();
+                document.getElementById("loading-message").style.display = "block";
 
-          return () => {
-            progressBar.setAttribute("value", "0");
-          };
-        }
-      },
+                const {
+                    paymentMethodId,
+                    issuerId,
+                    cardholderEmail: email,
+                    amount,
+                    token,
+                    installments,
+                    identificationNumber,
+                    identificationType,
+                } = cardForm.getCardFormData();
+
+                fetch("/process_payment", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({
+                        token,
+                        issuerId,
+                        paymentMethodId,
+                        transactionAmount: Number(amount),
+                        installments: Number(installments),
+                        description: productDescription,
+                        payer: {
+                            email,
+                            identification: {
+                                type: identificationType,
+                                number: identificationNumber,
+                            },
+                        },
+                    }),
+                })
+                    .then(response => {
+                        return response.json();
+                    })
+                    .then(result => {
+                        if(!result.hasOwnProperty("error_message")) {
+                            document.getElementById("success-response").style.display = "block";
+                            document.getElementById("payment-id").innerText = result.id;
+                            document.getElementById("payment-status").innerText = result.status;
+                            document.getElementById("payment-detail").innerText = result.detail;
+                        } else {
+                            document.getElementById("error-message").textContent = result.error_message;
+                            document.getElementById("fail-response").style.display = "block";
+                        }
+                        
+                        $('.container__payment').fadeOut(500);
+                        setTimeout(() => { $('.container__result').show(500).fadeIn(); }, 500);
+                    })
+                    .catch(error => {
+                        alert("Unexpected error\n"+JSON.stringify(error));
+                    });
+            },
+            onFetching: (resource) => {
+                console.log("Fetching resource: ", resource);
+                payButton.setAttribute('disabled', true);
+                return () => {
+                    payButton.removeAttribute("disabled");
+                };
+            },
+            onCardTokenReceived: (errorData, token) => {
+                if (errorData && errorData.error.fieldErrors.length !== 0) {
+                    errorData.error.fieldErrors.forEach(errorMessage => {
+                        alert(errorMessage);
+                    });
+                }
+
+                return token;
+            },
+            onValidityChange: (error, field) => {
+                const input = document.getElementById(form[field].id);
+                removeFieldErrorMessages(input, validationErrorMessages);
+                addFieldErrorMessages(input, validationErrorMessages, error);
+                enableOrDisablePayButton(validationErrorMessages, payButton);
+            }
+        },
     });
 
 </script>
